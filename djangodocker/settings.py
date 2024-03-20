efa2f654a7021a44c9e5537596d45efbf09cfc99
AZ <44141556+Tjiaz@users.noreply.github.com>
2024-03-20 23:55:49 +0000

    Update settings.py

--- djangodocker/settings.py
+++ djangodocker/settings.py
@@ -120,12 +120,12 @@ USE_TZ = True
 
 #sending emails
 
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='tunjiazeez24@gmail.com'
-EMAIL_HOST_PASSWORD='pqrw zbul phui dwbj'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST=
+EMAIL_HOST_USER=
+EMAIL_HOST_PASSWORD=
+EMAIL_PORT = 
+EMAIL_USE_TLS = 
+EMAIL_BACKEND = 
 
 
 
